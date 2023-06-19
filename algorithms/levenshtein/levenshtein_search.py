51b83984888bfbcccb5131ca889c9a333f96ba6e
Your Name <you@example.com>
2023-06-19 13:56:35 +0200

    implemented levenshtein search algo

---
+++ algorithms/levenshtein/levenshtein_search.py
@@ -0,0 +1,51 @@
+def levenshteinDistanceMatrix(s, t):
+    rows = len(s)+1
+    cols = len(t)+1
+    dist = [[0 for x in range(cols)] for x in range(rows)]
+
+    for i in range(1, rows):
+        dist[i][0] = i
+
+    for i in range(1, cols):
+        dist[0][i] = i
+        
+    for col in range(1, cols):
+        for row in range(1, rows):
+            if s[row-1] == t[col-1]:
+                cost = 0
+            else:
+                cost = 1
+            dist[row][col] = min(dist[row-1][col] + 1,      # deletion
+                                 dist[row][col-1] + 1,      # insertion
+                                 dist[row-1][col-1] + cost) # substitution
+    
+    return dist[row][col]
+
+
+
+def calcDictDistance(word, numWords):
+    file = open('mysite/polls/algorithms/1-1000.txt', 'r') 
+    lines = file.readlines() 
+    file.close()
+    dictWordDist = []
+    wordIdx = 0
+    
+    for line in lines: 
+        wordDistance = levenshteinDistanceMatrix(word, line.strip())
+        if wordDistance >= 10:
+            wordDistance = 9
+        dictWordDist.append(str(int(wordDistance)) + "-" + line.strip())
+        wordIdx = wordIdx + 1
+
+    closestWords = []
+    wordDetails = []
+    currWordDist = 0
+    dictWordDist.sort()
+    
+    for i in range(numWords):
+        currWordDist = dictWordDist[i]
+        wordDetails = currWordDist.split("-")
+        closestWords.append(wordDetails[1])
+    return closestWords
+
+print(calcDictDistance("pape", 3))
