e530f04c9cea4718193539e9d49ad74d3dbd099a
Your Name <you@example.com>
2023-06-26 13:25:24 +0200

    implemented levenshtein matrix test with 0 columns

--- tests/test_levenshtein_distance_matrix.py
+++ tests/test_levenshtein_distance_matrix.py
@@ -22,4 +22,27 @@ class levenshtein_Distance_Matrix_test:
             print()
 
     
+
+
+    def test_levenshtein_distance_matrix_with_0_columns_returns_fail():
+        # test init
+        rows = 0
+        cols = 0
+        distances = numpy.zeros((rows + 1, cols + 1))
+
+        # test execution
+
+        for t1 in range(rows + 1):
+            distances[t1][0] = t1
+
+        for t2 in range(cols + 1):
+            distances[0][t2] = t2
+
+        for col in range(cols + 1):
+            for row in range(rows + 1):
+                print(int(distances[col][row]), end=" ")
+            print()
+
+
 print(levenshtein_Distance_Matrix_test.test_levenshtein_distance_matrix_with_10_columns_returns_success()) 
+print(levenshtein_Distance_Matrix_test.test_levenshtein_distance_matrix_with_0_columns_returns_fail())
